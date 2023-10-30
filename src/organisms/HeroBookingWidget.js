236b1d9d818aefae35bc0091999cc33012e6c770
Rei Sikk <reix0010@stud.kea.dk>
2023-10-30 14:44:47 +0100

    hero stuff

---
+++ src/organisms/HeroBookingWidget.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './HeroBookingWidget.module.css'
+
+function HeroBookingWidget() {
+  return (
+    <div className={styles.booking_widget}>Hero booking widget</div>
+  )
+}
+
+export default HeroBookingWidget
---
+++ src/organisms/HeroSection.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeroBookingWidget from './HeroBookingWidget'
+import styles from './HeroSection.module.css'
+import Image from 'next/image'
+
+function HeroSection() {
+  return (
+      <div className={styles.wrapper}>
+        <HeroBookingWidget />
+        <div>
+            <Image src="https://cdn.dwarf.dk/comwell-cms-production/img/containers/main/kampagner/b2b_efter%C3%A5r2023/b2b_topheader.jpg/a1dbaeb00be6d3ed79294c38ccb1d729.webp" className={styles.image} alt="Hero image" width={1920} height={1080} />
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection

4f4056a42d6c62324c02eced1187044779595d00
Rei Sikk <reix0010@stud.kea.dk>
2023-10-31 16:26:51 +0100

    hero section css

--- src/organisms/HeroSection.js
+++ src/organisms/HeroSection.js
@@ -6,7 +6,9 @@ import Image from 'next/image'
 function HeroSection() {
   return (
       <div className={styles.wrapper}>
-        <HeroBookingWidget />
+        <div className={styles.booking_widget_layout}>
+           <HeroBookingWidget />
+        </div>
         <div>
             <Image src="https://cdn.dwarf.dk/comwell-cms-production/img/containers/main/kampagner/b2b_efter%C3%A5r2023/b2b_topheader.jpg/a1dbaeb00be6d3ed79294c38ccb1d729.webp" className={styles.image} alt="Hero image" width={1920} height={1080} />
         </div>

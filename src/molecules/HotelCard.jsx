2661a01567fd8903c85c1a66cfbf8f5c366bd681
Rei Sikk <reix0010@stud.kea.dk>
2023-11-09 13:04:17 +0100

    selected hotelCard styling

--- src/molecules/HotelCard.jsx
+++ src/molecules/HotelCard.jsx
@@ -3,8 +3,11 @@ import styles from './HotelCard.module.scss'
 
 
 function HotelCard({ hotel, updateSelectedHotel, selectedHotel }) {
+    const isSelected = selectedHotel && hotel._id === selectedHotel._id;
+    const cardClass = isSelected ? `${styles.hotel_card} ${styles.selected}` : styles.hotel_card;
+
   return (
-    <div className={styles.hotel_card}  onClick={() => updateSelectedHotel(hotel)}>
+    <div className={cardClass}  onClick={() => updateSelectedHotel(hotel)}>
         <div className={styles.hotel_img}>
 
         </div>

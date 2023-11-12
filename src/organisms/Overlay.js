76783dba7bba1faddb52c7fb5b0d58696a664c24
Rei Sikk <reix0010@stud.kea.dk>
2023-11-12 13:21:33 +0100

    overlay sorting and styling

--- src/organisms/Overlay.js
+++ src/organisms/Overlay.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Overlay.module.scss'
 import HotelCard from '../molecules/HotelCard'
+import { useState, useEffect } from 'react'
 
 function Overlay({ overlayState, updateOverlayState, hotelsData, updateSelectedHotel, selectedHotel  }) {
 
@@ -9,6 +10,24 @@ function Overlay({ overlayState, updateOverlayState, hotelsData, updateSelectedH
     "Choose room": "Guests & Rooms",
     "Check in / Check out": "Dates"
   };
+
+ const [selectedRegion, setSelectedRegion] = React.useState("All")
+  const handleLabelClick = (e) => {
+    setSelectedRegion(e.target.id);
+    //add class .selected to the clicked button and remove it from the others
+  }
+
+
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    setIsVisible(false);
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 250); // Delay showing the overlay to give the animation time to finish
+    
+    return () => 
+      clearTimeout(timer); // Clean up the timeout on unmount
+  }, [overlayState.overlayToShow, hotelsData, selectedRegion]);
 
   return (
     <div className={`${styles.overlay} ${overlayState.showOverlay ? styles.show : ''}`}>
@@ -19,10 +38,26 @@ function Overlay({ overlayState, updateOverlayState, hotelsData, updateSelectedH
                 <svg className={styles.close_icon} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16"><path stroke="currentColor" strokeWidth="1.5" d="M2.62 13.38 12.99 3.01M13.38 13.38 3.01 3.01"></path></svg>
                 </button>
             </div>
-            <div className={styles.overlay_data}>
-            {overlayState.overlayToShow === 'Choose hotel' && hotelsData && hotelsData.map((hotel) => (
-         <HotelCard hotel={hotel} key={hotel._id} selectedHotel={selectedHotel} updateSelectedHotel={updateSelectedHotel} />
-        ))}
+               <div className={styles.label_flex}>
+                  {overlayState.overlayToShow === 'Choose hotel' && (
+                    <>
+                   <button id='All' className={selectedRegion === 'All' ? styles.selected : ''} onClick={handleLabelClick}>All</button>
+                   <button id='Zealand' className={selectedRegion === 'Zealand' ? styles.selected : ''} onClick={handleLabelClick}>Zealand</button>
+                   <button id='Funen' className={selectedRegion === 'Funen' ? styles.selected : ''} onClick={handleLabelClick}>Funen</button>
+                  <button id='Jutland' className={selectedRegion === 'Jutland' ? styles.selected : ''} onClick={handleLabelClick}>Jutland</button>
+                    </>
+                  )
+                   }
+                </div>
+            <div className={`${styles.overlay_data} ${isVisible ? styles.visible : ''}`}>
+            {overlayState.overlayToShow === 'Choose hotel' && hotelsData && hotelsData
+              .filter(hotel => selectedRegion === 'All' || hotel.region === selectedRegion)
+              .map(hotel => ( <HotelCard 
+                hotel={hotel} key={hotel._id} 
+                selectedHotel={selectedHotel}
+                updateSelectedHotel={updateSelectedHotel} 
+                /> 
+           ))}
             </div>
             <div className={styles.drawer_bottom}>
               <button className={styles.drawer_lower_btn}>Select</button>

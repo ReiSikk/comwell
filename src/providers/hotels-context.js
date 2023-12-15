59aac897e6d0228dc72895c955e430729b5d124d
Rei Sikk <reix0010@stud.kea.dk>
2023-12-15 14:01:31 +0100

    inputs styling and conditional rendering

--- src/providers/hotels-context.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const HotelsContext = createContext();
-
-const useHotelsContext = () => useContext(HotelsContext);
-
-export default useHotelsContext;
